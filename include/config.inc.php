1d4d7c464870718da03a54e685ef5d7f80009768
NagypalMarton <43655988+NagypalMarton@users.noreply.github.com>
2020-05-02 23:47:44 +0200

    0502 - 04
    Galléria 1 (Nincs teljesen kész)

--- include/config.inc.php
+++ include/config.inc.php
@@ -17,14 +17,13 @@ $lablec = array(
 $oldalak = array(
 	'/' => array('fajl' => 'index', 'szoveg' => 'Kezdőlap', 'menun' => array(1,1)), //'menu' => 1(megjeleníti) V 0 (nem jeleníti meg)
 	'blog' => array('fajl' => 'blog', 'szoveg' => 'Blog', 'menun' => array(1,1)),
+	'gallery' => array('fajl' => 'gallery', 'szoveg' => 'Galéria', 'menun' => array(0,1)),
 	'elerhetoseg' => array('fajl' => 'elerhetoseg', 'szoveg' => 'Elérhetőség', 'menun' => array(1,1)),
 	'rolunk' => array('fajl' => 'aboutme', 'szoveg' => 'Rólunk','menun' => array(1,1)),
 	'belep-reg' => array('fajl' => 'belep-reg', 'szoveg' => 'Belépés/Regisztráció','menu' => array(1,0)),
 	'belepes' => array('fajl' => 'belepes', 'szoveg' => '', 'menun' => array(0,0)),
 	'kilepes' => array('fajl' => 'kilepes', 'szoveg' => 'Kilépés', 'menun' => array(0,1)),
 	'regisztracio' => array('fajl' => 'regisztracio', 'szoveg' => '', 'menun' => array(0,0))
-	//'projektjeink' => array('fajl' => 'projektjeink', 'szoveg' => 'Projektjeink','menun' => array(0,0)),
-	//'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat','menun' => array(0,0)),
 );
 
 $oldaldoboz=array(
@@ -32,4 +31,9 @@ $oldaldoboz=array(
 'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat','menun' => array(1,1)),
 );
 $hiba_oldal = array ('fajl' => '404', 'szoveg' => 'A keresett oldal nem található!');
+$MAPPA = 'users_img/';
+ $TIPUSOK = array ('.jpg', '.png');
+ $MEDIATIPUSOK = array('images/users_img/jpeg', 'images/users_img/png');
+ $DATUMFORMA = "Y.m.d. H:i";
+ $MAXMERET = 250*512;
 ?>
--- temp/pages/elerhetoseg.tpl.php
+++ temp/pages/elerhetoseg.tpl.php
@@ -1,18 +1,17 @@
 <div id="cim">
-Elérhetőségeink, avagy így jutathatod el a mondandódat!
+Elérhetőségeink
 </div>
 <div id="szoveg">
 <div id="contact1">
-<i><b>Álomfogó Alapítvány</b></i><br>
-6060 Tiszakécske, Kőrösi út 83.<br><br>
+<i><b>Országos Állatvédőrség Alapítvány</b></i><br>
+1117 Budapest, Nádorliget  u. 8./B. fsz.2.<br><br>
 
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2724.4599286104867!2d20.081766315185217!3d46.93299897914577!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4741589db1a1530f%3A0xc6c0a62d3b8782e2!2zVGlzemFrw6ljc2tlLCBLxZFyw7ZzaSB1LiA4MywgNjA2MA!5e0!3m2!1shu!2shu!4v1541289574040" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2697.157462299319!2d19.045696415147514!3d47.467360905580215!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4741dda5b6f4122f%3A0xa256895fd7663949!2zRXVyw7NwYSBTb2bFkXJzem9sZ8OhbGF0!5e0!3m2!1shu!2shu!4v1588453714835!5m2!1shu!2shu" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
 
-<br><br>Telefon: +36 76 440 273<br>
-Mobil: +36 20 551 32 61<br>
-E-mail: alomfogo@alomfogo.hu<br><br>
+<br><br><strong>Telefon:</strong> <br>+ 36 70 636 6230<br><br>
+<strong>E-mail:</strong><br><br> <strong>Általános témában: </strong>info@orszagosallatvedorseg.hu<br><strong>Bejelentés állatkínzás miatt: </strong>bejelentes@orszagosallatvedorseg.hu<br><br>
 
-Adószám: 18365919-1-03<br>
-Bankszámlaszám: 11732208-20038313
+<strong>Adószám:</strong> 18272011-2-43<br>
+<strong>Bankszámlaszám:</strong> 10100826 – 07126100 – 02003004 BUDAPEST BANK
 <div>
 </div>
