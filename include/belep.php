0916f0b734a9d2b645c59c918de16a379a145437
NagypalMarton <43655988+NagypalMarton@users.noreply.github.com>
2020-05-03 23:22:24 +0200

    0503 - 1.1.1
    belep.php update-elve

--- include/belep.php
+++ include/belep.php
@@ -7,7 +7,7 @@ if(isset($_POST['user_name']) && isset($_POST['user_passworld'])) {
         $dbh->query('SET NAMES utf8 COLLATE utf8_hungarian_ci');
         
         // Felhsználó keresése
-        $sqlSelect = "select id, vezeteknev, keresztnev from users where user_name = :user_name and user_passworld = sha1(:user_passworld)";
+        $sqlSelect = "select user_id, vezeteknev, keresztnev from users where user_name = :user_name and user_passworld = sha1(:user_passworld)";
         $sth = $dbh->prepare($sqlSelect);
         $sth->execute(array(':user_name' => $_POST['user_name'], ':user_passworld' => $_POST['user_passworld']));
         $row = $sth->fetch(PDO::FETCH_ASSOC);
