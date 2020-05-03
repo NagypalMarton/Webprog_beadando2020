77e9d3506bc0f0fa8e144c0143e864b122204a48
NagypalMarton <43655988+NagypalMarton@users.noreply.github.com>
2020-05-03 22:18:23 +0200

    0503 - 1

--- include/config.inc.php
+++ include/config.inc.php
@@ -17,13 +17,14 @@ $lablec = array(
 $oldalak = array(
 	'/' => array('fajl' => 'index', 'szoveg' => 'Kezdőlap', 'menun' => array(1,1)), //'menu' => 1(megjeleníti) V 0 (nem jeleníti meg)
 	'blog' => array('fajl' => 'blog', 'szoveg' => 'Blog', 'menun' => array(1,1)),
-	'gallery' => array('fajl' => 'gallery', 'szoveg' => 'Galéria', 'menun' => array(0,1)),
+	'gallery' => array('fajl' => 'gallery', 'szoveg' => 'Galéria', 'menun' => array(1,1)),
+	'feltolt' => array('fajl' => 'feltolt', 'szoveg' => 'Galéria', 'menun' => array(0,1)),
 	'elerhetoseg' => array('fajl' => 'elerhetoseg', 'szoveg' => 'Elérhetőség', 'menun' => array(1,1)),
 	'rolunk' => array('fajl' => 'aboutme', 'szoveg' => 'Rólunk','menun' => array(1,1)),
 	'belep-reg' => array('fajl' => 'belep-reg', 'szoveg' => 'Belépés/Regisztráció','menu' => array(1,0)),
-	'belepes' => array('fajl' => 'belepes', 'szoveg' => '', 'menun' => array(0,0)),
+	'belepes' => array('fajl' => 'belepes', 'szoveg' => '', 'menun' => array(1,0)),
 	'kilepes' => array('fajl' => 'kilepes', 'szoveg' => 'Kilépés', 'menun' => array(0,1)),
-	'regisztracio' => array('fajl' => 'regisztracio', 'szoveg' => '', 'menun' => array(0,0))
+	'regisztracio' => array('fajl' => 'regisztracio', 'szoveg' => '', 'menun' => array(1,0))
 );
 
 $oldaldoboz=array(
@@ -33,7 +34,7 @@ $oldaldoboz=array(
 $hiba_oldal = array ('fajl' => '404', 'szoveg' => 'A keresett oldal nem található!');
 $MAPPA = 'users_img/';
  $TIPUSOK = array ('.jpg', '.png');
- $MEDIATIPUSOK = array('images/users_img/jpeg', 'images/users_img/png');
+ $MEDIATIPUSOK = array('/images/users_img/jpeg', '/images/users_img/png');
  $DATUMFORMA = "Y.m.d. H:i";
  $MAXMERET = 250*512;
 ?>
