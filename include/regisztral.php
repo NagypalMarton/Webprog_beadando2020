c7789e15d895847156b5cc2d1f4e24395ce09b6b
NagypalMarton <43655988+NagypalMarton@users.noreply.github.com>
2020-05-03 23:18:26 +0200

    0503 - 1.1
    regisztral.php UPDATE-elve

--- include/regisztral.php
+++ include/regisztral.php
@@ -7,7 +7,7 @@ if(isset($_POST['user_name']) && isset($_POST['user_passworld']) && isset($_POST
         $dbh->query('SET NAMES utf8 COLLATE utf8_hungarian_ci');
         
         // Létezik már a felhasználói név?
-        $sqlSelect = "select id from users where user_name = :user_name";
+        $sqlSelect = "select user_id from users where user_name = :user_name";
         $sth = $dbh->prepare($sqlSelect);
         $sth->execute(array(':user_name' => $_POST['user_name']));
         if($row = $sth->fetch(PDO::FETCH_ASSOC)) {
@@ -16,7 +16,7 @@ if(isset($_POST['user_name']) && isset($_POST['user_passworld']) && isset($_POST
         }
         else {
             // Ha nem létezik, akkor regisztráljuk
-            $sqlInsert = "insert into users(id, vezeteknev, uto_nev, user_name, user_passworld)
+            $sqlInsert = "insert into users(user_id, vezeteknev, keresztnev, user_name, user_passworld)
                           values(0, :vezeteknev, :keresztnev, :user_name, :user_passworld)";
             $stmt = $dbh->prepare($sqlInsert); 
             $stmt->execute(array(':vezeteknev' => $_POST['vezeteknev'], ':keresztnev' => $_POST['keresztnev'],
