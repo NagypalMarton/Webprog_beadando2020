a5967a0195b51ac6710437ed6dc3ba31c53d37df
NagypalMarton <43655988+NagypalMarton@users.noreply.github.com>
2020-05-03 23:27:09 +0200

    0503 - 1.1.3
    Belépés/Kilépés és az adatbázis kapcsolat kész

---
+++ temp/pages/belepes.tpl.php
@@ -0,0 +1,23 @@
+    <form action = "?oldal=belep" method = "post">
+      <fieldset>
+        <legend>Bejlentkezés</legend>
+        <br>
+        <input type="text" name="user_name" placeholder="Felhasználónév" required><br><br>
+        <input type="password" name="user_passworld" placeholder="Jelszó" required><br><br>
+        <input type="submit" name="belepes" value="Belépés">
+        <br>&nbsp;
+      </fieldset>
+    </form>
+    <h3>Regisztrálja magát, ha még nem felhasználó!</h2>
+    <form action = "?oldal=regisztral" method = "post">
+      <fieldset>
+        <legend>Regisztráció</legend>
+        <br>
+        <input type="text" name="vezeteknev" placeholder="Vezetéknév" required><br><br>
+        <input type="text" name="keresztnev" placeholder="Keresztnév" required><br><br>
+        <input type="text" name="user_name" placeholder="Felhasználónév" required><br><br>
+        <input type="password" name="user_passworld" placeholder="Jelszó" required><br><br>
+        <input type="submit" name="regisztracio" value="Regisztráció">
+        <br>&nbsp;
+      </fieldset>
+    </form>
