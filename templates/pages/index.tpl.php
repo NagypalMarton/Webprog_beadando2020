4f5702e190304b396d1ec6205015e8ef44536be3
NagypalMarton <43655988+NagypalMarton@users.noreply.github.com>
2020-05-07 23:45:01 +0200

    0507 - 4.1

--- templates/pages/index.tpl.php
+++ templates/pages/index.tpl.php
@@ -12,11 +12,11 @@
 <p>Kérjük, hogy a megjegyzés rovatba tüntesd fel melyik mentvényünk támogatására küldöd az adományt.<br><br>
 Beállíthatsz rendszeres átutalást is, és így minden hónapban átutalja bankod a nekünk szánt összeget.<br>
 Amennyiben rendelkezel online/elektronikus banki hozzáféréssel, az alábbi linkek közül a bankodéra kattintva, azonnal be tudsz jelentkezni, s át tudod most utalni egyszerűen, a munkánk támogatására szánt adományt.</p>
-<a href="https://orszagosallatvedorseg.hu/" target="_blank">Látogassa meg régi oldalunkat!</a>
 <p><strong>Bankszámlaszámunk:</strong> BUDAPEST BANK 10100826 – 07126100 – 02003004</p>
 Támogatásukat nagyra értékeljük és köszönjük! Hivatalos dokumentációkat az alábbi képekre kattintva tudja letölteni!
 <div style="text-align:center;">
 <br><a href="./docs/Fejlesztői domumentáció 2020.docx">Fejlesztői dokumentáció</a>
-<br><a href="./docs/FTP.txt">FTP bejelentkezés</a>
+<br><a href="./docs/FTP.txt">FTP bejelentkezés</a><br>
+<a href="https://orszagosallatvedorseg.hu/" target="_blank">Látogassa meg régi oldalunkat!</a>
 </div>
 </div>
